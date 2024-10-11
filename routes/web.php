bb1d02e88fcfca706e64281fc5c02083508544cc
milton <miltondanielhch617@gmail.com>
2024-10-11 11:15:04 -0400

    autenticacion con gmail socialite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/auth/redirect', [AuthController::class, 'redirect'])->name('redirect');
+Route::get('/auth/callback-url', action: [AuthController::class, 'callback'])->name('callback');
